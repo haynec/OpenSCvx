dd28a0e8bb080e92bd4d5c5097c26faa68e6757d
griffin-norris <griffin.norris@scaledfoundations.ai>
2025-04-03 11:29:56 -0700

    add dynamics abstract class

---
+++ openscvx/dynamics.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod, ABC
+
+import jax
+import jax.numpy as jnp
+
+
+class Dynamics(ABC):
+    def __init__(self):
+
+        # CTCS Functions
+        self.g_jit = jax.jit(self.g_func)
+        self.g_vec = jax.jit(self.g_jit, in_axes=(0))
+
+        # Dynamics Functions
+        self.x_dot = jax.vmap(self.dynamics)
+        self.A = jax.jit(jax.vmap(jax.jacfwd(self.dynamics, argnums=0), in_axes=(0, 0)))
+        self.B = jax.jit(jax.vmap(jax.jacfwd(self.dynamics, argnums=1), in_axes=(0, 0)))
+
+    @abstractmethod
+    def dynamics(self, x: jnp.array, u: jnp.array) -> jnp.array:
+        pass
+
+    @abstractmethod
+    def g_func(self, x: jnp.array) -> jnp.array:
+        pass
+
+    @abstractmethod
+    def initial_trajectory(self):
+        # returns x_bar, u_bar
+        pass
