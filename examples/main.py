fbe3e07c5fc14637c7e5f74118a5a3be0a473910
Chris Hayner <58046076+haynec@users.noreply.github.com>
2025-04-04 17:03:22 -0400

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -7,7 +7,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from examples.params.dr_vp_polytope import params
+from examples.params.drone_racing import params
 
 from openscvx.ptr import PTR_main
 from openscvx.plotting import plot_camera_polytope_animation, plot_camera_animation, plot_animation, plot_scp_animation, plot_constraint_violation, plot_control, plot_state, plot_losses, plot_conic_view_animation, plot_camera_view
@@ -35,4 +35,4 @@ with open('results/results.pickle', 'wb') as f:
 with open('results/results.pickle', 'rb') as f:
     results = pickle.load(f) 
 
-# plot_animation(results, params)
+plot_animation(results, params)
