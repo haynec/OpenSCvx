c93741f3e9a714290111a66f26e69816817d1856
griffin-norris <griffin.norris@scaledfoundations.ai>
2025-04-08 17:32:47 -0700

    add decorators for constraint types

---
+++ openscvx/constraints/decorators.py
@@ -0,0 +1,11 @@
+import jax.numpy as jnp
+
+def ctcs(func: callable):
+    """Decorator to mark a function as a 'ctcs' constraint."""
+    func.constraint_type = "ctcs"
+    return func
+
+def nodal(func: callable):
+    """Decorator to mark a function as a 'nodal' constraint."""
+    func.constraint_type = "nodal"
+    return func
