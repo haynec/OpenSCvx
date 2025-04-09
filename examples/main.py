c90f391a1e0b9697f50b485ff471ca658bf3ad94
griffin-norris <griffin.norris@scaledfoundations.ai>
2025-04-09 14:28:36 -0700

    update main for post processing

--- examples/main.py
+++ examples/main.py
@@ -34,5 +34,6 @@ with open('results/results.pickle', 'wb') as f:
 with open('results/results.pickle', 'rb') as f:
     results = pickle.load(f) 
 
+# results = problem.post_process(results)
 # results.update(plotting_dict)
 # plot_animation(results, problem.params)
