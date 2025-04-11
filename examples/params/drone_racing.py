1b263745f4919de33fdab0fd39bdaeed8c8bb04b
Griffin Norris <norrisg@student.ethz.ch>
2025-04-10 21:46:21 -0700

    update examples

--- examples/params/drone_racing.py
+++ examples/params/drone_racing.py
@@ -76,6 +76,7 @@ for node, cen in zip(gate_nodes, A_gate_cen):
         nodal(
             lambda x, u, A=A_gate, c=cen: cp.norm(A @ x[:3] - c, "inf") <= 1,
             nodes=[node],
+            convex=True,
         )
     )  # use local variables inside the lambda function
 
