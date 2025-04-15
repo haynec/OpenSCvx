4d50660ba7c81146ea837b95429a3d56db4912ef
griffin-norris <griffin.norris@scaledfoundations.ai>
2025-04-15 14:40:34 -0700

    update examples

--- examples/params/obstacle_avoidance.py
+++ examples/params/obstacle_avoidance.py
@@ -104,7 +104,7 @@ problem.params.scp.w_tr_adapt = 1.8
 
 problem.params.sim.dt = 0.01
 problem.params.dis.custom_integrator = True
-problem.params.sim.cvxpygen = False
+problem.params.cvx.cvxpygen = False
 
 plotting_dict = dict(
     obstacles_centers=obstacle_centers,
