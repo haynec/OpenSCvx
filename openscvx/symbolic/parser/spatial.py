fd7af113d88032739c1db7d5e21805e5b6ae4819
Griffin Norris <164929321+griffin-norris@users.noreply.github.com>
2026-02-07 20:00:47 -0800

    add visitors for symbolic expression nodes

---
+++ openscvx/symbolic/parser/spatial.py
@@ -0,0 +1,28 @@
+"""Parser handlers for spatial / 6-DOF operations.
+
+Handlers: QDCM, SSM, SSMP
+"""
+
+from openscvx.symbolic.expr.spatial import QDCM, SSM, SSMP
+from openscvx.symbolic.parser._registry import function
+
+
+@function("QDCM")
+def _parse_qdcm(args, kwargs):
+    if len(args) != 1:
+        raise ValueError("QDCM() takes exactly 1 argument (quaternion)")
+    return QDCM(args[0])
+
+
+@function("SSM")
+def _parse_ssm(args, kwargs):
+    if len(args) != 1:
+        raise ValueError("SSM() takes exactly 1 argument (3D vector)")
+    return SSM(args[0])
+
+
+@function("SSMP")
+def _parse_ssmp(args, kwargs):
+    if len(args) != 1:
+        raise ValueError("SSMP() takes exactly 1 argument (angular velocity)")
+    return SSMP(args[0])
