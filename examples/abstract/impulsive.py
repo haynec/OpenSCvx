a54714066e7896d4caff05108dd753c2e27cc95d
spadafab <fabiospada1946@gmail.com>
2026-02-09 15:54:02 -0800

    [350_hybrid_support]: Updated example to hybrid control

--- examples/abstract/impulsive.py
+++ examples/abstract/impulsive.py
@@ -28,41 +28,49 @@ from openscvx.plotting import plot_controls, plot_states
 n = 10
 
 p = ox.State("position", shape=(1,))
-p.max = np.array([10.0])
+p.max = np.array([1.0])
 p.min = np.array([0.0])
 p.initial = np.array([0.0])
-p.final = np.array([10.0])
+p.final = np.array([1.0])
 p.guess = np.linspace(p.initial, p.final, n).reshape(-1, 1)
 
 v = ox.State("velocity", shape=(1,))  # Scalar speed
-v.max = np.array([10.0])
-v.min = np.array([-10.0])
+v.max = np.array([1.0])
+v.min = np.array([-1.0])
 v.initial = np.array([0.0])
 v.final = np.array([0.0])
 v.guess = np.linspace(v.initial, v.final, n).reshape(-1, 1)
 
-## TODO: (fabio) check about allocation components
-a = ox.Control("acceleration", shape=(1,), impulsive=True, allocation_matrix=np.array([[0], [1]]))
-a.max = np.array([10.0])
-a.min = np.array([-10.0])
-a.guess = np.linspace(a.max, a.min, n)
+dv = ox.Control("delta_v", shape=(1,), impulsive=True, allocation_matrix=np.array([[0], [1]]))
+dv.max = np.array([1.0])
+dv.min = np.array([-1.0])
+dv.guess = np.linspace(np.array([0]), np.array([0]), n)
+dv.scaling_min = np.array([-1])
+dv.scaling_max = np.array([1])
+
+a = ox.Control("acceleration", shape=(1,))
+a.max = np.array([0.01])
+a.min = np.array([-0.01])
+a.guess = np.linspace(np.array([0]), np.array([0]), n)
+a.scaling_min = np.array([-1])
+a.scaling_max = np.array([1])
 
 dynamics = {
     "position": v,
-    "velocity": 0,
+    "velocity": a,
 }
 
 states = [p, v]
-controls = [a]
+controls = [dv, a]
 
 constraints = []
 for state in states:
     constraints.extend([ox.ctcs(state <= state.max), ox.ctcs(state.min <= state)])
-# for i in range(n):
-#     if i != 0 and i != n-1:
-#         constraints.append((a.at(i) == 0).convex())
+for i in range(n):
+    if i != 1 and i != n - 1:
+        constraints.append((dv == 0.0).convex().at([i]))
 
-time = ox.Time(initial=0.0, final=ox.Minimize(10.0), min=0.0, max=100.0)
+time = ox.Time(initial=0.0, final=ox.Minimize(10.0), min=0.0, max=20.0)
 
 problem = Problem(
     dynamics=dynamics,
@@ -73,8 +81,9 @@ problem = Problem(
     N=n,
 )
 
-problem.algorithm.lam_prox = 1e0  # Weight on the Trust Reigon
-problem.algorithm.lam_cost = 1e-1  # Weight on the Minimal Time Objective
+problem.algorithm.lam_prox = 1e-2  # Weight on the Trust Reigon
+problem.algorithm.lam_vc = 1e1  # Weight on the Trust Reigon
+problem.algorithm.lam_cost = 1e0  # Weight on the Minimal Time Objective
 problem.algorithm.k_max = 100
 
 plotting_dict = {}
