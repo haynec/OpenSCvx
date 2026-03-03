34976632ab23591f83647006152800e531da864a
Griffin Norris <164929321+griffin-norris@users.noreply.github.com>
2026-03-03 09:14:32 -0800

    offset shifted `lag_sum`, `contour_sum` to remove constant term

--- examples/mpc/double_integrator_drone_racing.py
+++ examples/mpc/double_integrator_drone_racing.py
@@ -436,8 +436,22 @@ if __name__ == "__main__":
         position.guess = np.vstack([nodes["position"][1:], [ext_pos]])
         velocity.guess = np.vstack([nodes["velocity"][1:], [ext_vel]])
         progress.guess = shifted_progress
-        lag_sum.guess = np.vstack([nodes["lag_sum"][1:], nodes["lag_sum"][-1:]])
-        contour_sum.guess = np.vstack([nodes["contour_sum"][1:], nodes["contour_sum"][-1:]])
+        lag_offset = nodes["lag_sum"][1]
+        lag_sum.guess = np.maximum(
+            np.vstack([nodes["lag_sum"][1:] - lag_offset, nodes["lag_sum"][-1:] - lag_offset]),
+            0.0,
+        )
+
+        contour_offset = nodes["contour_sum"][1]
+        contour_sum.guess = np.maximum(
+            np.vstack(
+                [
+                    nodes["contour_sum"][1:] - contour_offset,
+                    nodes["contour_sum"][-1:] - contour_offset,
+                ]
+            ),
+            0.0,
+        )
 
         force.guess = np.vstack([nodes["force"][1:], [ext_force]])
         ext_speed = np.interp(ext_prog, s_data, speed_data)
